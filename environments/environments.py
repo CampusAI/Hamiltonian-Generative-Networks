aa5fe5174c768a47c9ab52da5cfab0098ac61a8c
charlio23 <carlesbalsellsrodas@hotmail.com>
2020-09-15 15:12:41 +0200

    created environment ABC, implemented pendulum draw method

---
+++ environments/environments.py
@@ -0,0 +1,67 @@
+from abc import ABC, abstractmethod
+from PIL import Image, ImageDraw
+import numpy as np
+
+class Environment(ABC):
+    
+    @abstractmethod
+    def set(self, p, q):
+        """Sets initial conditions for physical system
+
+        Args:
+            p (float): generalized momentum
+            q (float): generalized position
+
+        Raises:
+            NotImplementedError: Class instantiation has no implementation
+        """
+        raise NotImplementedError
+
+    def step(self, dt=0.01):
+        """Performs a step in the environment simulator
+
+        Args:
+            dt (float, optional): [description]. Defaults to 0.01.
+        """
+        raise NotImplementedError
+    
+    
+    def draw(self):
+        """Returns Caption of the environment state
+        """
+        raise NotImplementedError
+
+
+
+
+class Pendulum(Environment):
+
+    """Pendulum System
+
+    Equations of movement are:
+        
+        theta'' = -(g/l)*sin(theta)
+
+    """
+
+    def __init__(self, mass, length, p=None, q=None):
+        self.mass = mass
+        self.length = length
+        self.set(p,q)
+
+    def set(self, p, q):
+        self.p = p
+        self.q = q
+
+
+    def draw(self):
+        img = Image.new('L',(32, 32))
+        draw = ImageDraw.Draw(img)
+
+        r = self.mass
+        x = np.sin(self.q)*self.length + 32/2
+        y = np.cos(self.q)*self.length
+    
+        draw.ellipse((x-r, y-r, x+r, y+r), fill=255)
+
+        return np.array(img)
