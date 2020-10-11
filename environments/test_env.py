f45af17d7a83039fdfcf1229b5aee8f49d04d88e
OleguerCanal <oleguer.canal@hotmail.com>
2020-10-11 17:59:40 +0200

    Created test environment

---
+++ environments/test_env.py
@@ -0,0 +1,32 @@
+from .environments import Environment
+from PIL import Image, ImageDraw
+import numpy as np
+
+
+class TestEnv(Environment):
+    """Testing environment
+    """
+
+    def __init__(self, q=None, p=None):
+        self.set(p, q)
+
+    def set(self, q, p):
+        """Sets initial conditions for TestEnv
+
+        Args:
+            q ([float]): Generalized position in 1-D space
+            p ([float]): Generalized momentum in 1-D space
+        
+        Raises:
+            ValueError: If p and q are not in 1-D space
+        """
+        if len(p) != 1 or len(q) != 1:
+            raise ValueError("p and q must be in 1-D space: Angular momentum and Phase.")
+        self.p = p
+        self.q = q
+
+    def step(self, dt=1.0):
+        self.p += dt
+
+    def draw(self):
+        return self.p
