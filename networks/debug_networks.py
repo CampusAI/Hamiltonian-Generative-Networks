194753a3b7ece593821cc29be881c9851e9fa796
Federico <federico.taschin@studenti.unipd.it>
2020-10-11 18:11:21 +0200

    Fix debug_networks.py parameters not recognized

--- networks/debug_networks.py
+++ networks/debug_networks.py
@@ -1,13 +1,16 @@
 import torch
+from torch import nn
 from inference_net import to_phase_space
 
 
-class EncoderNet(torch.nn.Module):
+class EncoderNet(nn.Module):
 
     def __init__(self, phi=None, seq_len=2):
         super().__init__()
         self.seq_len = seq_len
-        self.phi = phi if phi is not None else torch.tensor([1., 1.], requires_grad=True)
+        self.phi = phi if phi is not None else nn.Parameter(
+            torch.tensor([1., 1.], requires_grad=True)
+        )
 
     def forward(self, x):
         """
@@ -30,7 +33,7 @@ class TransformerNet(torch.nn.Module):
 
     def __init__(self, w=None, sample=False):
         super().__init__()
-        self.w = w if w is not None else torch.tensor([1., 1.], requires_grad=True)
+        self.w = w if w is not None else nn.Parameter(torch.tensor([1., 1.], requires_grad=True))
 
     def forward(self, x):
         q_prime = self.w[0] * x[:, 0, :]
@@ -43,7 +46,9 @@ class HamiltonianNet(torch.nn.Module):
 
     def __init__(self, gamma=None):
         super().__init__()
-        self.gamma = gamma if gamma is not None else torch.tensor([1., 1.], requires_grad=True)
+        self.gamma = gamma if gamma is not None else nn.Parameter(
+            torch.tensor([1., 1.], requires_grad=True)
+        )
 
     def forward(self, q, p):
         return self.gamma[0] * q + self.gamma[1] * p ** 2
@@ -53,11 +58,10 @@ class DecoderNet(torch.nn.Module):
 
     def __init__(self, theta=None):
         super().__init__()
-        self.theta = theta if theta is not None else torch.tensor([2.])
+        self.theta = theta if theta is not None else nn.Parameter(torch.tensor([2.]))
 
     def forward(self, q):
         return self.theta * q
-
 
 
 if __name__ == '__main__':
