7143042dcf548836eb6c71efcc9534a991448c9b
Federico <federico.taschin@studenti.unipd.it>
2020-10-14 14:02:28 +0200

    Make networks fully parametrizable

--- networks/decoder_net.py
+++ networks/decoder_net.py
@@ -11,7 +11,7 @@ class ResidualBlock(nn.Module):
     """A residual block that up-samples the input image by a factor of 2.
     """
 
-    def __init__(self, in_channels, n_filters=64, kernel_size=3, padding='same'):
+    def __init__(self, in_channels, n_filters=64, kernel_size=3, padding='same', upsample=True):
         """Instantiate the residual block, composed by a 2x up-sampling and two convolutional
         layers.
 
@@ -21,10 +21,12 @@ class ResidualBlock(nn.Module):
             kernel_size (int): Size of the convolutional kernels.
             padding (Union[str, int]): Padding for the convolutional kernels. Can be an int,
                 or 'same' to automatically compute the padding to preserve the image size.
+            upsample (bool): Whether to upsample the image.
         """
         super().__init__()
         self.channels = in_channels
         self.n_filters = n_filters
+        self.upsample = upsample
         if isinstance(padding, int):
             use_padding = padding
         elif padding == 'same':
@@ -72,7 +74,8 @@ class ResidualBlock(nn.Module):
         Returns:
             A torch.Tensor with the upsampled images, of shape (N, n_filters, H, W).
         """
-        x = self.upsample(x)
+        if self.upsample:
+            x = self.upsample(x)
         residual = self.dim_match_conv(x) if self.channels != self.n_filters else x
         x = self.leaky_relu(self.conv1(x))
         x = self.leaky_relu(self.conv2(x))
@@ -87,23 +90,55 @@ class DecoderNet(nn.Module):
     of output channels, thus the output shape is (out_channels, H*2^3, W*2^3).
     """
 
-    def __init__(self, in_channels, out_channels, n_filters):
-        """Create the decoder network.
+    DEFAULT_PARAMS = {
+        'n_residual_blocks': 3,
+        'n_filters': [64, 64],
+        'kernel_sizes': [3, 3, 3],
+        'upsample': [True, True, True]
+    }
+
+    def __init__(self, in_channels, out_channels=3, n_residual_blocks=None, n_filters=None,
+                 kernel_sizes=None, upsample=None):
+        """Create the decoder network composed of the given number of residual blocks.
 
         Args:
             in_channels (int): Number of input encodings channels.
             out_channels (int): Number output image channels (1 for grayscale, 3 for RGB).
-            n_filters (int): Number of filters in the intermediate convolutional layers.
+            n_residual_blocks (int): Number of residual blocks in the network.
+            n_filters (list): List where the i-th element is the number of filters for
+                convolutional layers for the i-th residual block, excluding the output block.
+                Therefore, n_filters must be of length n_residual_blocks - 1
+            kernel_sizes(list): List where the i-th element is the kernel size of convolutional
+                layers for the i-th residual block.
+            upsample (list): List of booleans where the i-th element tells whether to upsample
+                the image in the i-th residual block.
         """
         super().__init__()
-        self.res1 = ResidualBlock(in_channels=in_channels, n_filters=n_filters)
-        self.res2 = ResidualBlock(in_channels=n_filters, n_filters=n_filters)
-        self.res3 = ResidualBlock(in_channels=n_filters, n_filters=n_filters)
-        self.out = nn.Conv2d(
-            in_channels=n_filters,
-            out_channels=out_channels,
-            kernel_size=1,
-            padding=0,
+        if all(var is None for var in(n_residual_blocks, n_filters, kernel_sizes, upsample)):
+            n_residual_blocks = DecoderNet.DEFAULT_PARAMS['n_residual_blocks']
+            n_filters = DecoderNet.DEFAULT_PARAMS['n_filters']
+            kernel_sizes = DecoderNet.DEFAULT_PARAMS['kernel_sizes']
+            upsample = DecoderNet.DEFAULT_PARAMS['upsample']
+        elif all(var is not None for var in(n_residual_blocks, n_filters, kernel_sizes, upsample)):
+            assert len(kernel_sizes) == n_residual_blocks and len(upsample) == n_residual_blocks,\
+                'kernel_sizes and upsample must be of length n_residual_blocks ('\
+                + str(n_residual_blocks) + ' in this case).'
+            assert len(n_filters) == n_residual_blocks - 1, 'n_filters must be of length ' \
+                'n_residual_blocks -1 (' + str(n_residual_blocks - 1) + ' in this case).'
+        else:
+            raise ValueError('Args n_residual_blocks, n_filters, kernel_size, upsample '
+                             'can only be either all None, or all defined by the user.')
+        filters = [in_channels] + n_filters + [out_channels]
+        self.residual_blocks = nn.ModuleList(
+            [
+                ResidualBlock(
+                    in_channels=filters[i],
+                    n_filters=filters[i+1],
+                    kernel_size=kernel_sizes[i],
+                    upsample=upsample[i]
+                )
+                for i in range(n_residual_blocks)
+            ]
         )
         self.sigmoid = nn.Sigmoid()
 
@@ -116,15 +151,14 @@ class DecoderNet(nn.Module):
         Returns:
             Tensor of shape (out_channels, H * 2^3, W * 2^3) with the reconstructed image.
         """
-        x = self.res1(x)
-        x = self.res2(x)
-        x = self.res3(x)
-        x = self.sigmoid(self.out(x))
+        for layer in self.residual_blocks:
+            x = layer(x)
+        x = self.sigmoid(x)
         return x
 
 
 if __name__ == '__main__':
-    decoder = DecoderNet(in_channels=16, out_channels=3, n_filters=64)
+    decoder = DecoderNet(in_channels=16, out_channels=3)
     inp = torch.randn((1, 16, 4, 4))
     out = decoder(inp)
     print(out.size())
--- networks/hamiltonian_net.py
+++ networks/hamiltonian_net.py
@@ -11,46 +11,106 @@ class HamiltonianNet(nn.Module):
     """The Hamiltonian network, composed of 6 convolutional layers and a final linear layer.
     """
 
-    def __init__(self, in_channels, dtype=torch.float):
+    DEFAULT_PARAMS = {
+        'hidden_conv_layers': 6,
+        'n_filters': [32, 64, 64, 64, 64, 64, 64, 64],
+        'kernel_sizes': [3, 3, 3, 3, 3, 3, 3, 3, 3],
+        'strides': [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    }
+
+    def __init__(self, in_shape, hidden_conv_layers=None, n_filters=None, kernel_sizes=None,
+                 strides=None, act_func=nn.ReLU(), dtype=torch.float):
         """Create the layers of the Hamiltonian network.
 
+        If K is the total number of convolutional layers, then hidden_conv_layers = K - 2.
+        The length of n_filters, kernel_sizes, and strides must be K. If all of them are None,
+        HamiltonianNet.DEFAULT_PARAMS will be used.
+
         Args:
-            in_channels (int): Number of input in_channels.
+            in_shape (tuple): Shape of input elements (channels, height, width).
+            hidden_conv_layers (int): Number of hidden convolutional layers (excluding the input and
+                the two output layers for mean and variance).
+            n_filters (list): List with number of filters for each of the hidden layers.
+            kernel_sizes (list): List with kernel sizes for each convolutional layer.
+            strides (list): List with strides for each convolutional layer.
+            act_func (torch.nn.Module): The activation function to apply after each layer.
             dtype (torch.dtype): Type of the weights.
         """
         super().__init__()
-        self.num_flat_features = 64 * 4 * 4
-        self.in_conv = nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=3, padding=1)
-        self.conv1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        self.out_conv = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        self.linear = nn.Linear(in_features=self.num_flat_features, out_features=1)
+        if all(var is None for var in (hidden_conv_layers, n_filters, kernel_sizes, strides)):
+            hidden_conv_layers = HamiltonianNet.DEFAULT_PARAMS['hidden_conv_layers']
+            n_filters = HamiltonianNet.DEFAULT_PARAMS['n_filters']
+            kernel_sizes = HamiltonianNet.DEFAULT_PARAMS['kernel_sizes']
+            strides = HamiltonianNet.DEFAULT_PARAMS['strides']
+        elif all(var is not None for var in (hidden_conv_layers, n_filters, kernel_sizes, strides)):
+            # If no Nones, check consistency
+            assert len(n_filters) == hidden_conv_layers + 2,\
+                'n_filters must be a list of length hidden_conv_layers + 2 ' \
+                '(' + str(hidden_conv_layers + 2) + ' in this case).'
+            assert len(kernel_sizes) == hidden_conv_layers + 2 and \
+                   len(strides) == hidden_conv_layers + 2, \
+                   'kernel_sizes and strides must be lists with values foreach layer in the ' \
+                   'network (' + str(hidden_conv_layers + 2) + ' in this case).'
+        else:
+            raise ValueError('Args hidden_conv_layers, n_filters, kernel_sizes, and strides'
+                             'can only be either all None, or all defined by the user.')
+        in_channels = in_shape[0] * 2
+        paddings = [int(k/2) for k in kernel_sizes]
+        self.in_conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=n_filters[0],
+            kernel_size=kernel_sizes[0],
+            padding=paddings[0])
+        out_size = int((in_shape[1] - kernel_sizes[0] + 2 * paddings[0])) / strides[0] + 1
+        self.hidden_layers = nn.ModuleList(
+            modules=[
+                nn.Conv2d(
+                    in_channels=n_filters[i],
+                    out_channels=n_filters[i + 1],
+                    kernel_size=kernel_sizes[i + 1],
+                    padding=paddings[i + 1],
+                    stride=strides[i + 1]
+                )
+                for i in range(hidden_conv_layers)
+            ]
+        )
+        for i in range(hidden_conv_layers):
+            out_size = int((out_size - kernel_sizes[i] + 2 * paddings[i]) / strides[i]) + 1
+        self.out_conv = nn.Conv2d(
+            in_channels=n_filters[-1],
+            out_channels=n_filters[-1],
+            kernel_size=kernel_sizes[-1],
+            padding=paddings[-1]
+        )
+        out_size = int((out_size - kernel_sizes[-1] + 2 * paddings[-1]) / strides[-1]) + 1
+        self.n_flat = out_size ** 2 * n_filters[-1]
+        self.linear = nn.Linear(in_features=self.n_flat, out_features=1)
+        self.activation = act_func
         self.type(dtype)
 
     def forward(self, q, p):
         """Forward pass that returns the Hamiltonian for the given q and p inputs.
 
-        q and p must be two N x C x H x W tensors, where N is the batch size, C the number of
-        in_channels, H and W the height and width.
-
-        TODO: Should we use activations such as ReLU or LeakyReLU?
+        q and p must be two (batch_size, channels, height, width) tensors.
 
         Args:
             q (torch.Tensor): The tensor corresponding to the position in abstract space.
             p (torch.Tensor): The tensor corresponding to the momentum in abstract space.
 
         Returns:
-            A N x 1 tensor with the Hamiltonian for each input in the batch.
+            A (batch_size, 1) shaped tensor with the Hamiltonian for each input in the batch.
         """
         x = torch.cat((q, p), dim=1)  # Concatenate q and p to obtain a N x 2C x H x W tensor
-        x = self.in_conv(x)
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.out_conv(x)
-        x = x.view(-1, self.num_flat_features)
+        x = self.activation(self.in_conv(x))
+        for layer in self.hidden_layers:
+            x = self.activation(layer(x))
+        x = self.activation(self.out_conv(x))
+        x = x.view(-1, self.n_flat)
         x = self.linear(x)
         return x
+
+
+if __name__ == '__main__':
+    hamiltonian_net = HamiltonianNet(in_shape=(16, 4, 4))
+    q, p = torch.randn((2, 128, 16, 4, 4))
+    h = hamiltonian_net(q, p)
