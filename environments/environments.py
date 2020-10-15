4639ae680ce3c8c2b7b5309ca5756ad3fd86e9ae
OleguerCanal <oleguer.canal@hotmail.com>
2020-10-15 10:33:40 +0200

    Autopep

--- environments/environments.py
+++ environments/environments.py
@@ -85,7 +85,16 @@ class Environment(ABC):
         y0 = np.array([np.array(self.q), np.array(self.p)]).reshape(-1)
         self._rollout = solve_ivp(self._dynamics, t_span, y0, t_eval=t_eval).y
 
-    def sample_random_rollouts(self, number_of_frames=100, delta_time=0.1, number_of_rollouts=16, img_size=32, color=True, noise_std=0.1, radius_bound=(1.3, 2.3), world_size=1.5, seed=None):
+    def sample_random_rollouts(self,
+                               number_of_frames=100,
+                               delta_time=0.1,
+                               number_of_rollouts=16,
+                               img_size=32,
+                               color=True,
+                               noise_std=0.1,
+                               radius_bound=(1.3, 2.3),
+                               world_size=1.5,
+                               seed=None):
         """Samples random rollouts for a given environment
 
         Args:
@@ -113,7 +122,7 @@ class Environment(ABC):
         total_time = number_of_frames * delta_time
         batch_sample = []
         for i in range(number_of_rollouts):
-            radius = np.random.rand()*(radius_ub - radius_lb) + radius_lb
+            radius = np.random.rand() * (radius_ub - radius_lb) + radius_lb
             self._sample_init_conditions(radius)
             self._evolution(total_time, delta_time)
             if noise_std > 0.:
@@ -135,6 +144,9 @@ def visualize_rollout(rollout):
     img = []
     for im in rollout:
         img.append([plt.imshow(im, animated=True)])
-        ani = animation.ArtistAnimation(fig, img, interval=50,
-                                        blit=True, repeat_delay=1000)
+        ani = animation.ArtistAnimation(fig,
+                                        img,
+                                        interval=50,
+                                        blit=True,
+                                        repeat_delay=1000)
     plt.show()
