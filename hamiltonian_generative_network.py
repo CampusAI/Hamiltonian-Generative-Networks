4c31e876c8259f0c11d5c63fa4bf9c147ca06981
Carles Balsells <carlesbalsellsrodas@hotmail.com>
2020-10-16 20:53:12 +0000

    bugfix cuda tensor to numpy array, line 127

--- hamiltonian_generative_network.py
+++ hamiltonian_generative_network.py
@@ -124,7 +124,7 @@ class HGN():
         # Optimization step
         error.backward()
         self.optimizer.step()
-        return float(reconstruction_error.detach().numpy()), float(KLD.detach().numpy())
+        return float(reconstruction_error.detach().cpu().numpy()), float(KLD.detach().cpu().numpy())
 
     def load(self, directory):
         """Load networks' parameters
