003b94322f45ebebac11b1e575e2d05253cdff30
OleguerCanal <oleguer.canal@hotmail.com>
2020-10-16 12:32:07 +0200

    Implemented dataloader

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import os
 import torch
 import yaml
 
+from environments.datasets import EnvironmentSampler
 from environments.environment_factory import EnvFactory
 from hamiltonian_generative_network import HGN
 from networks.inference_net import EncoderNet, TransformerNet
@@ -73,16 +74,27 @@ if __name__ == "__main__":
               seq_len=params["rollout"]["seq_length"],
               channels=params["rollout"]["n_channels"])
 
-    # for i in range(params["optimization"]["epochs"]):  # For each training step
-    #     # Sample rollouts from the environment NOTE: This will be changed into the dataloader once its implemented
-    #     rollouts = env.sample_random_rollouts(
-    #         seed=i,
-    #         number_of_frames=params["rollout"]["seq_length"],
-    #         delta_time=params["rollout"]["delta_time"],
-    #         number_of_rollouts=params["optimization"]["batch_size"],
-    #         color=(params["rollout"]["n_channels"]==3))
+    # Dataloader
+    trainDS = EnvironmentSampler(environment=env,
+                                 dataset_len=100,
+                                 number_of_frames=params["rollout"]["seq_length"],
+                                 delta_time=params["rollout"]["delta_time"],
+                                 number_of_rollouts=params["optimization"]["batch_size"],
+                                 img_size=params["dataset"]["img_size"],
+                                 noise_std=params["dataset"]["noise_std"],
+                                 radius_bound=params["dataset"]["radius_bound"],
+                                 world_size=params["dataset"]["world_size"],
+                                 seed=None)
+    # Dataloader instance test, batch_mode disabled
+    train = torch.utils.data.DataLoader(trainDS,
+                                        shuffle=False,
+                                        batch_size=None)
+    errors = []
+    for rollout_batch in train:
+        rollout_batch = rollout_batch.float()
+        error = hgn.fit(rollout_batch)
+        errors.append(error)
+    
+    print("errors:\n", errors)
 
-    #     error = hgn.fit(rollouts)
-    #     break
-
-hgn.save(os.path.join(params["model_save_dir"], params["experiment_id"]))
+    hgn.save(os.path.join(params["model_save_dir"], params["experiment_id"]))
