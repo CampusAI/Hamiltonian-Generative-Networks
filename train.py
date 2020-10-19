e52608962a04a2839ce60f1aebb860213b33408b
Federico <federico.taschin@studenti.unipd.it>
2020-10-19 12:11:23 +0200

    Add dtype to train.py and refactor

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import time
 import torch
 import tqdm
 import yaml
+import numpy as np
 
 from environments.datasets import EnvironmentSampler
 from environments.environment import visualize_rollout
@@ -18,14 +19,13 @@ from utilities.integrator import Integrator
 from utilities.training_logger import TrainingLogger
 
 
-
-def train(params):
-    """Instantiate and train the HGN.
+def train(params, dtype=torch.float):
+    """Instantiate and train the Hamiltonian Generative Network.
 
     Args:
-        params (dictionary): Experiment parameters (see experiment_params folder)
+        params (dict): Experiment parameters (see experiment_params folder)
     """
-     # Set device
+    # Set device
     device = "cuda:" + str(
         params["gpu_id"]) if torch.cuda.is_available() else "cpu"
 
@@ -33,17 +33,17 @@ def train(params):
     env = EnvFactory.get_environment(**params["environment"])
 
     # Instantiate networks
-    encoder = EncoderNet(seq_len=params["rollout"]["seq_length"],
-                         in_channels=params["rollout"]["n_channels"],
-                         **params["networks"]["encoder"]).to(device)
+    encoder = EncoderNet(
+        seq_len=params["rollout"]["seq_length"], in_channels=params["rollout"]["n_channels"],
+        **params["networks"]["encoder"], dtype=dtype).to(device)
     transformer = TransformerNet(
         in_channels=params["networks"]["encoder"]["out_channels"],
-        **params["networks"]["transformer"]).to(device)
-    hnn = HamiltonianNet(**params["networks"]["hamiltonian"]).to(device)
+        **params["networks"]["transformer"], dtype=dtype).to(device)
+    hnn = HamiltonianNet(**params["networks"]["hamiltonian"], dtype=dtype).to(device)
     decoder = DecoderNet(
         in_channels=params["networks"]["transformer"]["out_channels"],
-        out_channels=params["rollout"]["n_channels"],
-        **params["networks"]["decoder"]).to(device)
+        out_channels=params["rollout"]["n_channels"], **params["networks"]["decoder"],
+        dtype=dtype).to(device)
 
     # Define HGN integrator
     integrator = Integrator(delta_t=params["rollout"]["delta_time"],
@@ -83,8 +83,7 @@ def train(params):
               channels=params["rollout"]["n_channels"])
 
     # Dataloader
-    dataset_len = params["optimization"]["epochs"] * params["optimization"][
-        "batch_size"]
+    dataset_len = params["optimization"]["epochs"] * params["optimization"]["batch_size"]
     seed = None if params["dataset"]["random"] else 0
     trainDS = EnvironmentSampler(
         environment=env,
@@ -97,7 +96,8 @@ def train(params):
         noise_std=params["dataset"]["noise_std"],
         radius_bound=params["dataset"]["radius_bound"],
         world_size=params["dataset"]["world_size"],
-        seed=seed)
+        seed=seed,
+        dtype=dtype)
     # Dataloader instance test, batch_mode disabled
     data_loader = torch.utils.data.DataLoader(trainDS,
                                               shuffle=False,
@@ -111,12 +111,13 @@ def train(params):
     # Initialize tensorboard writer
     pbar = tqdm.tqdm(data_loader)
     for i, rollout_batch in enumerate(pbar):
-        rollout_batch = rollout_batch.float().to(device)
+        # rollout_batch has shape (batch_len, seq_len, channels, height, width)
+        rollout_batch = rollout_batch.to(device)
         error, kld, prediction = hgn.fit(rollout_batch)
         training_logger.step(losses=(error, kld),
                              rollout_batch=rollout_batch,
                              prediction=prediction)
-        msg = "Loss: %s, KL: %s" % (round(error, 4), round(kld, 4))
+        msg = "Loss: %s, KL: %s" % (np.round(error, 4), np.round(kld, 4))
         pbar.set_description(msg)
     hgn.save(os.path.join(params["model_save_dir"], params["experiment_id"]))
 
@@ -129,4 +130,4 @@ if __name__ == "__main__":
         params = yaml.load(f, Loader=yaml.FullLoader)
     
     # Train HGN network
-    train(params)
+    train(params, dtype=torch.double)
