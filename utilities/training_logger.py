de01715ec80f0eed9bea4e8f6ea4be5615ac9e27
Federico <federico.taschin@studenti.unipd.it>
2020-10-19 11:30:30 +0200

    Fix TrainingLogger video shape

--- utilities/training_logger.py
+++ utilities/training_logger.py
@@ -4,13 +4,16 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 class TrainingLogger:
+
     def __init__(self, hyper_params, loss_freq=100, rollout_freq=1000):
-        """Instantiate a Traning Logger
+        """Instantiate a TrainingLogger.
 
         Args:
-            hyper_params (dict): Parameters used to train the model (for reproducibility)
-            loss_freq (int, optional): Frequency at which the loss values are updated in TensorBoard. Defaults to 100.
-            rollout_freq (int, optional): Frequency at which videos are updated in TensorBoard. Defaults to 1000.
+            hyper_params (dict): Parameters used to train the model (for reproducibility).
+            loss_freq (int, optional): Frequency at which the loss values are updated in
+                TensorBoard. Defaults to 100.
+            rollout_freq (int, optional): Frequency at which videos are updated in TensorBoard.
+                Defaults to 1000.
         """
         self.writer = SummaryWriter(log_dir=os.path.join("runs", hyper_params["experiment_id"]))
         self.writer.add_text('data/hyperparams', str(hyper_params), 0)
@@ -22,9 +25,11 @@ class TrainingLogger:
         """Perform a logging step: Update inner iteration counter and log info if needed
 
         Args:
-            losses (tuple(float, float)): Tuple of reconstruction loss and KLD
-            rollout_batch (torch.Tensor(N, C, H, W)): Image sequence of the system evolution concatenated along the channels' axis inputed to the model
-            prediction (HGNResult): Prediction given by the HGN model
+            losses (tuple): Tuple of two floats, corresponding to reconstruction loss and KLD.
+            rollout_batch (torch.Tensor): Batch of ground truth rollouts, as a Tensor of shape
+                (batch_size, seq_len, channels, height, width).
+            prediction (utilities.hgn_result.HgnResult): The HgnResult object containing data of
+                the models forward pass on the rollout_batch.
         """
         if self.iteration % self.loss_freq == 0:
             self.writer.add_scalar('data/reconstruction_loss', losses[0],
@@ -36,7 +41,6 @@ class TrainingLogger:
                                   rollout_batch.detach().cpu(), self.iteration)
             self.writer.add_video(
                 'data/reconstruction',
-                prediction.reconstructed_rollout.unsqueeze(2).detach().cpu(),
+                prediction.reconstructed_rollout.detach().cpu(),
                 self.iteration)
-
         self.iteration += 1
