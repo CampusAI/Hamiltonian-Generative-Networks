512e22b81a4e286d2b4b1243416174cede635045
Stathi Fotiadis <sfotiadis@gmail.com>
2020-11-21 21:24:45 +0200

    more thorough kld tests

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -6,10 +6,18 @@ from utilities.losses import kld_loss
 
 def test_kld_loss():
     batch_sizes = [1, 10, 100]
-    latent_size = 10
-    for batch_size in batch_sizes:
+    latent_size = 8
+    for batch_size in [10]: #batch_sizes:
         mu = torch.randn((batch_size, latent_size))
         logvar = torch.randn((batch_size, latent_size))
 
         kld = kld_loss(mu, logvar)
-        assert len(kld.size()) == 1
+        assert kld.dim() == 0
+
+        mu = torch.randn((batch_size, latent_size, latent_size, latent_size))
+        logvar = torch.randn((batch_size, latent_size, latent_size, latent_size))
+
+        kld = kld_loss(mu, logvar)
+        assert kld.dim() == 0
+
+test_kld_loss()
